55e0305a1f540b9783560c3f2b7b1a84091fed24
emirsassan <endwse@gmail.com>
2024-05-09 11:01:09 +0300

    fix tests

--- tests/arrays.test.ts
+++ tests/arrays.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "bun:test";
 import { readTecoConfig } from "..";
 
 test("Parsing arrays by default", () => {
-    readTecoConfig("./arrayConfig.teco").then((x: any) => {
-        expect(x).toBe({ hello: ["world"] })
+    readTecoConfig("./tests/arrayConfig.teco").then((x: any) => {
+        expect(x).toEqual({ hello: ["world"] })
     })
 })
--- tests/config.test.ts
+++ tests/config.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "bun:test";
 import { readTecoConfig } from "..";
 
 test("Config file main test", () => {
-    readTecoConfig("./mainConfig.teco").then((x: any) => {
-        expect(x).toBe({ hello: "world" })
+    readTecoConfig("./tests/mainConfig.teco").then((x: any) => {
+        expect(x).toEqual({ hello: "world" })
     })
 })
